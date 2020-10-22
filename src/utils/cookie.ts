b0fb0c8a664d9324d99947121f85031223ae6769
Brantley <brantley@sodiumhalogen.com>
2020-10-21 19:12:31 -0500

    📦  NEW: updates cookies utils page

--- src/utils/cookie.ts
+++ src/utils/cookie.ts
@@ -1,28 +1,23 @@
+import Cookies from 'universal-cookie';
+
+
 export const setCookie = (cname: string, cvalue: string, exdays: number) => {
+  const cookies = new Cookies();
   const d = new Date();
   d.setTime(d.getTime() + exdays * 24 * 60 * 60 * 1000);
-  const expires = `expires=${d.toUTCString()}`;
-  document.cookie = `${cname}=${cvalue};${expires};path=/`;
+  cookies.set(cname, cvalue, { path: '/' });
 };
 
-export const getCookie = (cname: string) => {
-  const name = `${cname}=`;
-  const decodedCookie = decodeURIComponent(document.cookie);
-  const ca = decodedCookie.split(";");
-  for (let i = 0; i < ca.length; i++) {
-    let c = ca[i];
-    while (c.charAt(0) === " ") {
-      c = c.substring(1);
-    }
-    if (c.indexOf(name) === 0) {
-      return c.substring(name.length, c.length);
-    }
-  }
-  return "";
+export const getCookie = async (cname: string) => {
+  const cookies = new Cookies();
+  const cookie = await cookies.get(cname);
+  return cookie;
+
 };
 
 export const deleteCookie = (cname: string) => {
-  document.cookie = cname + "=; expires=Thu, 01 Jan 1970 00:00:01 GMT;";
+  const cookies = new Cookies();
+  cookies.remove(cname);
 };
 
 export default deleteCookie;
