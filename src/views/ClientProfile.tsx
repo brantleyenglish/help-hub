bad0c3b3a53725cce6074f5493278265023ff2f1
Christen Harper <christenbarber1231@gmail.com>
2020-11-10 19:27:08 -0600

    edits

--- src/views/ClientProfile.tsx
+++ src/views/ClientProfile.tsx
@@ -1,6 +1,10 @@
 import React from "react";
+import { ErrorMessage, Field, Form, Formik } from "formik";
+import styled from "styled-components";
 import ClientHd from "../components/clientProfile/clientHeader";
 import SmallHead from "../components/clientProfile/timelineToggle";
+
+
 
 class ClientProfile extends React.Component {
   render() {
