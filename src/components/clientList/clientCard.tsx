552826612258f5e2ea2b60b6a984f802cdc8904b
Brantley <brantley@sodiumhalogen.com>
2020-11-17 19:03:50 -0600

    📝  CONTENT: adds client data to client card

--- src/components/clientList/clientCard.tsx
+++ src/components/clientList/clientCard.tsx
@@ -1,56 +1,33 @@
-import React from "react";
-import {
-    faHeartbeat,
-    faTshirt,
-    faUtensils,
-    faHome,
-    faBookReader,
-    faWheelchair,
-    faHandsHelping,
-    faQuestion,
-    faSearch,
-    faPhone,
-    faMapMarkerAlt
-} from "@fortawesome/free-solid-svg-icons";
+import { faMapMarkerAlt, faPhone } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import React from "react";
 
-class ClientCard extends React.Component {
-    render() {
-        return (
-            <div className="bigClientDiv">
-                <div className="clientholder">
-                    <div className="left">
-                        <h1>John Smith </h1>
-                        <h2>
-                            DOB: <p>3/17/1968</p>
-                        </h2>
-                        <h2>
-                            {" "}
-                            <FontAwesomeIcon icon={faPhone} />
-                            <p>555-555-5555</p>
-                        </h2>
-                        <h2>
-                            <FontAwesomeIcon icon={faMapMarkerAlt} />{" "}
-                            <p>
-                                541 Wiley Parker Rd. <br /> Jackson, TN 38305
-              </p>
-                        </h2>
-                    </div>
-                    <br />
-                    <div className="right">
-                        <h2>
-                            {" "}
-              Gender: <p>Male</p>
-                        </h2>
-                        <h2>
-                            {" "}
-              Ethnicity: <p>Caucasian</p>{" "}
-                        </h2>
-                    </div>
-                </div>
-            </div>
-        );
-    }
-}
+const ClientCard: React.FC<any> = ({ client }) => {
+  return (
+    <div className="bigClientDiv">
+      <div className="clientholder">
+        <div className="left">
+          <h1>{`${client?.clientFirstName} ${client?.clientLastName}`}</h1>
+          <h2>
+            DOB: <p>{client?.dob}</p>
+          </h2>
+          <h2>
+            <FontAwesomeIcon icon={faPhone} />
+            <p>{client?.phone}</p>
+          </h2>
+          <h2>
+            <FontAwesomeIcon icon={faMapMarkerAlt} />{" "}
+            <p>{`${client?.address} ${client?.city}, TN ${client?.zip}`}</p>
+          </h2>
+        </div>
+        <br />
+        <div className="right">
+          <h2>Gender: {client?.gender}</h2>
+          <h2>Ethnicity: {client?.ethnicity}</h2>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default ClientCard;
