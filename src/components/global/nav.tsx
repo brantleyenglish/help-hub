33e62bfb7bb98c117ede73d1a868223c80784bfa
Christen Harper <christenbarber1231@gmail.com>
2020-12-01 20:18:59 -0600

    Added dynamic nav (log in/profile link)

--- src/components/global/nav.tsx
+++ src/components/global/nav.tsx
@@ -1,8 +1,13 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import styled from "styled-components";
-import { theme } from "../../components/Theme";
+import { theme } from "../Theme";
+import { useAssistance } from "../../context/AssistanceContext";
 import UnitedWayLogo from "../../images/uw_logo.png";
+import ProfilePic from "../../images/helphubPlaceholder.png";
+import { useAuth } from "../../context/AuthContext";
+import { useAgency } from "../../context/AgencyContext";
+
 
 const NavWrapper = styled.div`
   background-color: #f2f2f2;
@@ -30,6 +35,28 @@ const NavLinkWrapper = styled.div`
     color: #333333;
   }
 `;
+const ProfileNavLinkWrapper = styled.div`
+  margin-left: 25px;
+  margin-right: 25px;
+  padding-top: 1vw;
+  padding-bottom: 0.3vw;
+  padding-left: 0.5vw;
+  padding-right: 0.5vw;
+  font-size: 15px;
+  color: #999999;
+  text-transform: uppercase;
+  border-bottom: 2px ${theme.colors.lightBlue} solid;
+  &:hover {
+    color: #333333;
+  }
+  // & img{
+  //   width: 20px;
+  //   height: 20px;
+  //   border-radius: 100%;
+  //   margin: 0;
+  //   padding: 0;
+  }
+`;
 const NavSpanWrapper = styled.span`
   position: absolute;
   right: 0;
@@ -37,7 +64,14 @@ const NavSpanWrapper = styled.span`
   flex-direction: row;
 `;
 
+
+
 const Nav = () => {
+
+  const { setAssistanceAgencyId } = useAssistance();
+  const { user } = useAuth();
+  const { agency } = useAgency();
+
   return (
     <NavWrapper>
       <Link to="/">
@@ -57,9 +91,25 @@ const Nav = () => {
         <Link to="/clients">
           <NavLinkWrapper>Clients</NavLinkWrapper>
         </Link>
-        <Link to="/login">
-          <NavLinkWrapper>Log In</NavLinkWrapper>
-        </Link>
+        {!user && (
+          <Link to="/login">
+            <NavLinkWrapper>Log In</NavLinkWrapper>
+          </Link>
+        )}
+        {user && (
+          <Link
+            to={`/agencies/${user?.uid}`}
+            key={user?.uid}
+            onClick={() =>
+              setAssistanceAgencyId ? setAssistanceAgencyId(user?.uid) : null
+            }
+          >
+            <ProfileNavLinkWrapper>
+              {/* <img src={ProfilePic} alt="" /> */}
+              {agency?.name}
+            </ProfileNavLinkWrapper>
+          </Link>
+        )}
       </NavSpanWrapper>
     </NavWrapper>
   );
