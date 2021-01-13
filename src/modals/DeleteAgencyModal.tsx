7b1d5fa89b755be89dd64953e4915c0bf1e8f182
Christen Harper <christenbarber1231@gmail.com>
2021-01-12 18:49:58 -0600

    kdfsa;l

---
+++ src/modals/DeleteAgencyModal.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const DeleteAgencyModal = () => {
+    return <></>;
+};
+
+export default DeleteAgencyModal;
---
+++ src/modals/DeleteMessageModal.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const DeleteMessageModal = () => {
+    return <></>;
+};
+
+export default DeleteMessageModal;
