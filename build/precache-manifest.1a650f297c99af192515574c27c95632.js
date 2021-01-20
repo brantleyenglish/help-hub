d2ef5e8e5fc7b40d48eebd48652f7084fce14932
Brantley <brantley@sodiumhalogen.com>
2021-01-19 20:07:44 -0600

    📦  FEAT: adds build folder to repo

---
+++ build/precache-manifest.1a650f297c99af192515574c27c95632.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b20f3993f19afe3aee79ba7f498feeb8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3d7ba3e140562e05b1d2",
+    "url": "/static/css/main.01538168.chunk.css"
+  },
+  {
+    "revision": "1b811f5b612a02baa097",
+    "url": "/static/js/2.a3955deb.chunk.js"
+  },
+  {
+    "revision": "ff7452d9cff6430bfd21f22f595aa6c6",
+    "url": "/static/js/2.a3955deb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3d7ba3e140562e05b1d2",
+    "url": "/static/js/main.78c2a7f7.chunk.js"
+  },
+  {
+    "revision": "2747b914e9a60db2276f",
+    "url": "/static/js/runtime-main.c8a21426.js"
+  },
+  {
+    "revision": "ccd7642e8dbecceccfbb80d193f114a8",
+    "url": "/static/media/helphubPlaceholder.ccd7642e.png"
+  },
+  {
+    "revision": "a8e22445f55f1596f905e259ef7e66ab",
+    "url": "/static/media/uw_header.a8e22445.png"
+  },
+  {
+    "revision": "c2e1df11d81b9a54510688d6b559e0a4",
+    "url": "/static/media/uw_logo.c2e1df11.png"
+  }
+]);
