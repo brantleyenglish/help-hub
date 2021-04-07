1230add684e5fe1ab27fb70afb2733f35f21761f
Christen Harper <christenbarber1231@gmail.com>
2021-04-06 19:13:58 -0500

    jkl

---
+++ build/precache-manifest.abfae07b8f0f6d9a01ea5316a95129ca.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "35d78f64f80c85e28c9e6dac83636c68",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fb8cb812354b5b637369",
+    "url": "/static/css/main.01538168.chunk.css"
+  },
+  {
+    "revision": "4ff89f0924b45900b705",
+    "url": "/static/js/2.210250df.chunk.js"
+  },
+  {
+    "revision": "6f5a4c1d502183b394a22ca773bc6f97",
+    "url": "/static/js/2.210250df.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fb8cb812354b5b637369",
+    "url": "/static/js/main.31b32fc2.chunk.js"
+  },
+  {
+    "revision": "2747b914e9a60db2276f",
+    "url": "/static/js/runtime-main.c8a21426.js"
+  },
+  {
+    "revision": "6067b6b1c9853d14ec80635a9a9806b5",
+    "url": "/static/media/Logo211UW.6067b6b1.png"
+  },
+  {
+    "revision": "ccd7642e8dbecceccfbb80d193f114a8",
+    "url": "/static/media/helphubPlaceholder.ccd7642e.png"
+  },
+  {
+    "revision": "a8e22445f55f1596f905e259ef7e66ab",
+    "url": "/static/media/uw_header.a8e22445.png"
+  }
+]);
